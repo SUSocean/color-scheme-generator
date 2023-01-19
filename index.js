812b826357913f859d45b30084f0d2c618ab7517
SUSocean <igonind.a1@gmail.com>
2023-01-19 16:11:11 +0200

    add copy function

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ document.addEventListener('submit', function (e) {
     fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}`)
         .then(res => res.json())
         .then(data => (data.colors).forEach(color => {
-            colorsHTML += `<div style='background: ${color.hex.value};' class='color-column'><span>${color.hex.value}</span></div>`
+            colorsHTML += `<div style='background: ${color.hex.value};' class='color-column'><p id='c${color.hex.value}' class='copied-text'>COPIED</p><span class="hex" id='${color.hex.value}'>${color.hex.value}</span></div>`
         }))
         .then(data => document.getElementById('color-container').innerHTML = colorsHTML)
+})
+
+document.addEventListener('click', function (e) {
+    if (e.target.classList.value === 'hex') {
+        navigator.clipboard.writeText(document.getElementById(e.target.id).textContent)
+        document.getElementById(`c${e.target.id}`).style.animationName = 'copy-text'
+        setTimeout(() => document.getElementById(`c${e.target.id}`).style.animationName = '', 2000)
+    }
 })
